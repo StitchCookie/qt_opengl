2131aa2e5f9002572d3c0beafac95a97e8065650
StitchCookie <stitchcool@163.com>
2024-03-19 14:40:47 +0800

    优化网格代码

---
+++ copenglwidget.h
@@ -0,0 +1,104 @@
+#ifndef COPENGLWIDGET_H
+#define COPENGLWIDGET_H
+
+// openGL相关库
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions_3_3_Core>
+#include <QOpenGLShaderProgram>
+#include <QTimer>
+#include <QElapsedTimer>
+#include <QOpenGLTexture>
+#include <QKeyEvent>
+#include <QMouseEvent>
+#include <QWheelEvent>
+#include <QVector3D>
+#include "camera.h"
+#include <QOpenGLTexture>
+
+#include "mesh.h"
+
+class COpenGlWidget : public QOpenGLWidget,QOpenGLFunctions_3_3_Core
+{
+    Q_OBJECT
+public:
+    explicit COpenGlWidget(QWidget *parent = nullptr);
+    ~COpenGlWidget() override;
+    enum Shape{None,Rect,Circle,Triangle};
+    void drawShape(Shape shape);
+    void setWrirFame(bool wireFrame);
+    float vertices[288] = {
+        // positions          // normals           // texture coords
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
+    };
+    QMatrix4x4 calculate_lookAt_matrix(QVector3D position,QVector3D target,QVector3D worldUp);
+
+protected:
+    /**
+     * @brief slots_timeout 让光照在场景中移动
+     */
+    void slots_timeout();
+
+protected:
+    virtual void initializeGL() override;
+    virtual void resizeGL(int w, int h) override;
+    virtual void paintGL() override;
+    bool eventFilter(QObject *watched, QEvent *event)override;
+    void mouseMoveEvent(QMouseEvent *ev) override;
+    void wheelEvent(QWheelEvent *event)override;
+private:
+    Shape m_shape;
+    QOpenGLShaderProgram shaderProgramObject; // 创建相关的对象
+    QOpenGLShaderProgram shaderProgramLighting;
+    bool flag;
+    unsigned int VAO,VBO,lightVAO;
+    QElapsedTimer  timerElapsed;
+    QTimer         timer;
+
+    QOpenGLTexture *m_Texture;
+    QOpenGLTexture *m_TextureSpec;
+    Camera          m_camera;
+    Mesh            *m_mash;
+    Mesh* processMesh();
+};
+
+#endif // COPENGLWIDGET_H
---
+++ mainwindow.cpp
@@ -0,0 +1,30 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_action_Rect_triggered()
+{
+    ui->openGLWidget->drawShape(COpenGlWidget::Rect);
+}
+
+void MainWindow::on_action_clear_triggered()
+{
+    ui->openGLWidget->drawShape(COpenGlWidget::None);
+}
+
+void MainWindow::on_action_xuanran_triggered()
+{
+    ui->openGLWidget->setWrirFame(ui->action_xuanran->isChecked());
+}
